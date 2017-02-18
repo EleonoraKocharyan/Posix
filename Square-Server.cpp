f6768ded59ae8585c0883c6a3beeb3b5a475d683
Eleonora Kocharyan <eleonora.kocharyan@gmail.com>
2017-02-18 12:07:08 +0400

    Update Square-Server.cpp

--- Square-Server.cpp
+++ Square-Server.cpp
@@ -14,77 +14,76 @@ int main()
 
 	*i=-1;//when -1 parent works, when 0 child works
     
-	ofstream out;
+    ofstream out;
     ifstream in;
 	
-	pid_t pid;
-	pid=fork();
-
+    pid_t pid;
+    pid=fork();
+	
     if(pid!=0) //if parent
-	{
+    {
 		while(1)
 		{   //number input
-        	int x; 
+			int x; 
             cout<<"enter n = ";
-			cin>>x;
-            //
+		    cin>>x;
+			
 			if(x!=0)
-            {
+			{
 				while(1)
-                if(*i==-1)
-                {   //writing input number in the file
-                	out.open("iofile.txt" );
-                    out<<x;
-                    out.close();
-                    //
-                    *i=0; //giving the child permission to work
-                    break;
-                }
-
-                while(1)
-                if(*i==-1)
-                {   //reading the square of input
-                    int n;
-                    in.open("iofile.txt");
-                    in>>n;
-                    in.close();
-                    //
-                    cout<<"n^2 = "<<n<<endl; //printing the result
-                    break;
-                }
-            }
-            else //input was 0
-            {
-                kill(pid,SIGKILL); //killing processes
-                return 0;
-            }      
+					if(*i==-1)
+					{   //writing input number in the file
+						out.open("iofile.txt" );
+                	    out<<x;
+               		    out.close();
+						
+						*i=0; //giving the child permission to work
+           		         break;
+                	}
+					
+				while(1)
+                	if(*i==-1)
+                	{   //reading the square of input
+						int n;
+                    	in.open("iofile.txt");
+                  	    in>>n;
+                        in.close();
+                    
+                    	cout<<"n^2 = "<<n<<endl; //printing the result
+                        break;
+               		 }
+			}
+			
+			else //input was 0
+			{
+				kill(pid,SIGKILL); //killing processes
+				return 0;
+			}
 		}
-    }
-        
+	}
+	
 	else
-    {
-
-        while(1)
-        {
-            while(1)
-            if(*i==0)
-            {   //reading the input number from the file 
-				int n;
-                in.open( "iofile.txt" );
-                in>>n;
-                in.close();
-                //writing square of input number in the file
-                n*=n;
-            	out.open("iofile.txt");
-                out<<n;
-                out.close();
-                //
-                *i=-1; //giving the parent permission to work
-                break;
-
-            }
-        }
-    }
+	{
+		while(1)
+		{
+			while(1)
+				if(*i==0)
+				{   //reading the input number from the file 
+					int n;
+					in.open( "iofile.txt" );
+					in>>n;
+					in.close();
+					//writing square of input number in the file
+					n*=n;
+            				out.open("iofile.txt");
+					out<<n;
+					out.close();
+               
+					*i=-1; //giving the parent permission to work
+					break;
+				}
+		}
+	}
 }
 
 
